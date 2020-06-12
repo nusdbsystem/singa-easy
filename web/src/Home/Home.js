6ee6c5ebd5483d695dc5a8fbdd16e83f46edf52a
ArielCLJ <arielclj@gmail.com>
2020-06-12 17:00:06 +0800

    Included background for landing page

--- web/src/Home/Home.js
+++ web/src/Home/Home.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 import "./Home.css";
 import Particles from 'react-particles-js';
+
+const particlesOptions = {
+    "particles": {
+        "line_linked": {
+            "enable": true,
+            "color": "#615555",
+            "width": 0.5
+
+        },
+        "color": { value: "#000000" },
+        "number": {
+            "value": 50
+        },
+        "size": { "value": 3 },
+
+    }
+};
+
 export default class Home extends Component {
     render() {
         return (
+
             <div className="Home">
                 <div className="lander">
+                    <Particles className="particles" params={particlesOptions} />
                     <h1>Predictions</h1>
                     <p>To insert a brief description of Singa-Easy Models and objective of application</p>
                 </div>
             </div>
-            
-            
+
         );
     }
 }
